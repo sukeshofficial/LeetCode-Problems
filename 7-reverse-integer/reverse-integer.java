d27169cdc803515d02e3b81fd9ea47715b17b8e0
SUKESH D <sukesh.official.2006@gmail.com>
2025-06-11 06:15:18 +0530

    Time: 1 ms (83.37%) | Memory: 40.7 MB (93.62%) - LeetSync

--- 7-reverse-integer/reverse-integer.java
+++ 7-reverse-integer/reverse-integer.java
@@ -1,24 +1,22 @@
 class Solution {
     public int reverse(int x) {
-        int sum = 0;
-        while (x != 0) {
-            int digit = x % 10;
-            x /= 10;
+        int rev = 0;
+        while(x != 0){
+            if(rev > Integer.MAX_VALUE/10) return 0;
+            if(rev < Integer.MIN_VALUE/10) return 0;
 
+            int rem = x % 10;
+            rev = (rev * 10) + rem;
             // overflow
             /* 
                 Given a signed 32-bit integer x, return x with its digits reversed. 
                 If reversing x causes the value to go **outside the signed 32-bit integer** range 
                 [-231, 231 - 1], then return 0.
             */
-            if (sum > Integer.MAX_VALUE / 10 || (sum == Integer.MAX_VALUE / 10 && digit > 7)) 
-                return 0;
-            if (sum < Integer.MIN_VALUE / 10 || (sum == Integer.MIN_VALUE / 10 && digit < -8)) 
-                return 0;
-
-
-            sum = (sum * 10) + digit;
+            x = x / 10;
         }
-        return sum;
+
+        return rev;
+
     }
 }
