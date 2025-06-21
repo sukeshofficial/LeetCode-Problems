8b8569e0fa7da77c3ef0ecfd42903ca2cfedfe19
SUKESH D <sukesh.official.2006@gmail.com>
2025-06-21 10:42:32 +0530

    Time: 1 ms (99.84%) | Memory: 53 MB (65.85%) - LeetSync

---
+++ 169-majority-element/majority-element.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        // Boyer-Moore Voting Algorithm
+        int count = 0;
+        int candidate = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+
+            count += (candidate == num) ? 1 : -1;
+        }
+
+        return candidate;
+
+    }
+}
