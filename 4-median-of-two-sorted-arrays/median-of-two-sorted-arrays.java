5cff2568c3c2e184924acca96e716afa17f520d7
SUKESH D <sukesh.official.2006@gmail.com>
2026-07-01 20:47:15 +0530

    Time: 1 ms (100.00%) | Memory: 48.8 MB (71.25%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -5,28 +5,44 @@ class Solution {
         int mArrLen = n1Len + n2Len;
         // Merge array
         int[] mergedArray = new int[n1Len + n2Len];
-        if (n1Len > 0) {
-            for (int i = 0; i < n1Len; i++) {
-                mergedArray[i] = nums1[i];
-            }
-        }
-        if (n2Len > 0) {
-            for (int i = 0; i < n2Len; i++) {
-                mergedArray[i + n1Len] = nums2[i];
-            }
-        }
+        // if (n1Len > 0) {
+        //     for (int i = 0; i < n1Len; i++) {
+        //         mergedArray[i] = nums1[i];
+        //     }
+        // }
+        // if (n2Len > 0) {
+        //     for (int i = 0; i < n2Len; i++) {
+        //         mergedArray[i + n1Len] = nums2[i];
+        //     }
+        // }
 
-        // Sort
-        if (mArrLen > 1) {
-            for (int i = 0; i < mArrLen; i++) {
-                for (int j = i + 1; j < mArrLen; j++) {
-                    if (mergedArray[i] > mergedArray[j]) {
-                        int temp = mergedArray[i];
-                        mergedArray[i] = mergedArray[j];
-                        mergedArray[j] = temp;
-                    }
-                }
+        // // Sort
+        // if (mArrLen > 1) {
+        //     for (int i = 0; i < mArrLen; i++) {
+        //         for (int j = i + 1; j < mArrLen; j++) {
+        //             if (mergedArray[i] > mergedArray[j]) {
+        //                 int temp = mergedArray[i];
+        //                 mergedArray[i] = mergedArray[j];
+        //                 mergedArray[j] = temp;
+        //             }
+        //         }
+        //     }
+        // }
+
+        int i=0, j=0, k=0;
+
+        while(i < n1Len && j < n2Len) {
+            if(nums1[i] < nums2[j]) {
+                mergedArray[k++] = nums1[i++]; 
+            } else {
+                mergedArray[k++] = nums2[j++];
             }
+        }
+        while(i < n1Len) {
+            mergedArray[k++] = nums1[i++];
+        }
+        while(j < n2Len) {
+            mergedArray[k++] = nums2[j++];
         }
 
         if (mArrLen % 2 != 0) {
