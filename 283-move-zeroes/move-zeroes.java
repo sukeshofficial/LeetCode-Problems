ef560ba4eacf69ae2bc577e600114f849b795663
SUKESH D <sukesh.official.2006@gmail.com>
2026-08-14 07:07:57 +0530

    Time: 2 ms (91.97%) | Memory: 47.8 MB (38.70%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,25 +1,13 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int j = -1;
-        int n = nums.length;
-        // if nums[i] == 0 point the j variable to this ith index
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == 0) {
-                j = i;
-                break;
-            }
-        }
+        int left = 0;
 
-        // if j value is not changed then there are no zero element
-        if (j == -1)
-            return;
-
-        for (int i = j + 1; i < n; i++) {
-            if (nums[i] != 0) {
-                int temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
-                j++;
+        for(int right = 0; right < nums.length; right++) {
+            if(nums[right] != 0) {
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+                left++; 
             }
         }
     }
